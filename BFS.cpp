249f574d8383c1f12a1889595587f5aac2e486a7
Zhe Zhang <zhe-zhan22@mails.tsinghua.edu.cn>
2024-03-11 11:26:13 +0800

    add checkbox for forbidding night train

--- BFS.cpp
+++ BFS.cpp
@@ -14,21 +14,22 @@
 #define BADLIM 2.0
 using namespace std;
 int tot, head[N], h, t;
+stack<int> stw;
 stack<string> st;
 map<string, int> mp;
 vector<string> message;
 struct Edge {
     int v, w, nxt;
     string info;
-    bool T;
+    bool T, Y;
 } edge[M];
 struct QElem {
-    int u, depth, from;
+    int u, depth, from, w;
     string info;
 } queue[20000005];
 struct Node {
     vector<string> resinfo;
-    int restime;
+    int restime, estitime;
 };
 vector<Node> res;
 int timecalc(string st, string ed) {
@@ -52,8 +53,8 @@ void Stringsplit(const string &str, const string &split, vector<string> &res) {
     delete[] strc;
 }
 
-void addedge(int u, int v, int w, string info) {
-    edge[++tot] = (Edge){v, w, head[u], info, false};
+void addedge(int u, int v, int w, string info, char y) {
+    edge[++tot] = (Edge){v, w, head[u], info, false, y == 'Y' ? true : false};
     head[u] = tot;
     if (info == "T")
         edge[tot].T = true;
@@ -62,9 +63,10 @@ void backward(int x) {
     if (x == -1)
         return;
     st.push(queue[x].info);
+    stw.push(queue[x].w);
     backward(queue[x].from);
 }
-void BFS(int S, int T, bool Ttag) {
+void BFS(int S, int T, bool Ttag, bool Ytag) {
     h = 1;
     t = 0;
     queue[++t] = QElem{S, 0, -1};
@@ -74,20 +76,27 @@ void BFS(int S, int T, bool Ttag) {
         for (int i = head[u.u]; i; i = edge[i].nxt) {
             if (edge[i].T && !Ttag)
                 continue;
+            if (edge[i].Y && !Ytag)
+                continue;
             int v = edge[i].v;
             if (v == u.from)
                 continue;
             else if (v == T) {
                 message.clear();
                 st.push(edge[i].info);
+                stw.push(edge[i].w);
                 backward(h - 1);
                 while (!st.empty()) {
                     message.push_back(st.top());
                     st.pop();
                 }
-                res.push_back(Node{message, 0});
+                res.push_back(Node{message, 0, 0});
+                while (!stw.empty()) {
+                    res[res.size() - 1].estitime += stw.top();
+                    stw.pop();
+                }
             } else
-                queue[++t] = QElem{v, u.depth + 1, h - 1, edge[i].info};
+                queue[++t] = QElem{v, u.depth + 1, h - 1, edge[i].w, edge[i].info};
         }
     }
 }
@@ -104,14 +113,15 @@ int main() {
     }
     int u, v, w;
     string info;
-    while (cin >> u >> v >> w >> info) {
-        addedge(u, v, w, info);
+    char y;
+    while (cin >> u >> v >> w >> info >> y) {
+        addedge(u, v, w, info, y);
     }
     end = clock();
     cout << "Time: " << (double)(end - start) / CLOCKS_PER_SEC << "s" << endl;
 
-    string ST = "临潼", ED = "北京西";
-    bool T = true;
+    string ST = "隆回", ED = "北京西";
+    bool T = false, Y = true;
 
     res.clear();
     vector<string> ststations, edstations;
@@ -124,7 +134,7 @@ int main() {
             int st = mp[ststations[i]], ed = mp[edstations[j]];
             for (int i = st * SPLIT * 2 + SPLIT; i < (st + 1) * SPLIT * 2; i++) {
                 for (int j = ed * SPLIT * 2; j < ed * SPLIT * 2 + SPLIT; j++) {
-                    BFS(i, j, T);
+                    BFS(i, j, T, Y);
                 }
             }
         }
@@ -142,6 +152,8 @@ int main() {
             res[i].restime += timecalc(stime, etime);
             lastime = etime;
         }
+        while (res[i].restime < res[i].estitime)
+            res[i].restime += 24 * 60;
     }
     sort(res.begin(), res.end(), [](Node a, Node b) { return a.restime < b.restime; });
     for (int i = 0; i < res.size(); i++) {
